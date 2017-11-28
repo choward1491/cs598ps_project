4474c4118752da9a1d6183f769eade49fe5820d9
Sameer Manchanda <smanchan96@users.noreply.github.com>
2017-11-27 18:09:01 -0600

    Add files via upload

--- scripts/coarsen.py
+++ scripts/coarsen.py
@@ -5,20 +5,38 @@ Created on Fri Oct  6 10:20:25 2017
 @author: manchan2
 """
 
-
 import numpy as np
 import nibabel as nib
 import os
+import pickle
+
+class Scan():
+    def __init__(self, isControl, personId, runNo, data):
+        self.isControl=isControl
+        self.personId=personId
+        self.runNo=runNo
+        self.data=data
+        
+output = open('C:\\Users\\schan\\Documents\\LargeFiles\\coarse.pkl', 'wb')
 #import matplotlib.pyplot as plt
 #from sklearn.decomposition import NMF 
 import time
-def runData(subj, run):
-    suffix="_bold.nii.gz"
-    directory = "C:\\Users\\schan\\OneDrive\\Documents\\Homework\\Fall 2017\\MLSP\\project\\ds171_R1.0.0_controls\\ds171_R1.0.0_controls\\ds171_R1.0.0\\sub-control"+str(subj).zfill(2)+"\\func"
-    files = [x for x in os.listdir(directory) if x.endswith(suffix)]
+def runData(isControl, subj, run):
+    if isControl:
+        suffix="_bold.nii.gz"
+        directory = "C:\\Users\\schan\\Documents\\LargeFiles\\ds171_R1.0.0\\sub-control"+str(subj).zfill(2)+"\\func"
+        files = [x for x in os.listdir(directory) if x.endswith(suffix)]
         img=nib.load(directory+"\\"+sorted(files)[run-1])
-    data=img.get_data()
-    return(data)
+        data=img.get_data()
+        return(data)
+    else:
+        suffix="_bold.nii.gz"
+        directory = "C:\\Users\\schan\\Documents\\LargeFiles\\ds171_R1.0.0_mdd\\ds171_R1.0.0\\sub-mdd"+str(subj).zfill(2)+"\\func"
+        files = [x for x in os.listdir(directory) if x.endswith(suffix)]
+        img=nib.load(directory+"\\"+sorted(files)[run-1])
+        data=img.get_data()
+        return(data)
+
 
 def coarsen(data):
     assert(len(data.shape)==3)
@@ -31,9 +49,21 @@ def coarsen(data):
 start = time.time()
 for i in range(20):
     for j in range(5):
-        rd = runData(i+1,j+1)
+        rd = runData(True, i+1,j+1)
         d = np.asarray([coarsen(rd[:,:,:,i]) for i in range(rd.shape[3])])
-        np.save("subj"+str(i+1).zfill(2)+"_task"+str(j+1)+".npy",d)
+        scan = Scan(True, i+1, j+1, d)
+        pickle.dump(scan, output, pickle.HIGHEST_PROTOCOL)
+        print(i,j)
+end = time.time()
+print(end-start)
+
+start = time.time()
+for i in range(19):
+    for j in range(5):
+        rd = runData(False, i+1,j+1)
+        d = np.asarray([coarsen(rd[:,:,:,i]) for i in range(rd.shape[3])])
+        scan = Scan(False, i+1, j+1, d)
+        pickle.dump(scan, output, pickle.HIGHEST_PROTOCOL)
         print(i,j)
 end = time.time()
 print(end-start)
---
+++ scripts/feature_extraction.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 27 09:28:41 2017
+
+@author: schan
+"""
+
+import pickle
+import numpy as np
+from sklearn.decomposition import PCA, NMF
+
+class Weights():
+    def __init__(self, scan):
+        self.isControl = scan.isControl
+        self.personId=scan.personId
+        self.runNo=scan.runNo
+        self.weights=0
+    def setWeights(self, weights):
+        self.weights=weights
+
+mat= np.zeros(shape=(0, 16*16*10))
+weightsArr=[]
+arr = []
+f = open('C:\\Users\\schan\\Documents\\LargeFiles\\coarse.pkl', 'rb')
+while True:
+    try:
+        scan = pickle.load(f)
+    except:
+        break
+    d=scan.data
+    arr.append(d.shape[0])
+    weightsArr.append(Weights(scan))
+    mat = np.append(mat, np.reshape(d, (d.shape[0], d.shape[1]*d.shape[2]*d.shape[3])), axis=0)
+    if len(arr)%20 == 0:
+        print(len(arr))
+
+print(mat.shape)
+np.save("C:\\Users\\schan\\Documents\\LargeFiles\\mat.npy", mat)
+
+doPCA=False
+doNMF=True
+if doPCA:
+    pca = PCA(n_components=700, whiten=True)
+    pca.fit(mat)
+    
+    print("PCA Fitted")
+    curr = 0
+    for i,x in enumerate(arr):
+        weights = pca.transform(mat[curr:(curr+x), :])
+        curr+=x
+        weightsArr[i].setWeights(weights)
+    
+    f = open('C:\\Users\\schan\\Documents\\LargeFiles\\pca.pkl', 'wb')
+    pickle.dump(weightsArr,f,pickle.HIGHEST_PROTOCOL)
+    pickle.dump(pca.components_, f, pickle.HIGHEST_PROTOCOL)
+if doNMF:
+    nmf = NMF(n_components=20)
+    nmf.fit(mat)
+    
+    print("NMF Fitted")
+    curr = 0
+    for i,x in enumerate(arr):
+        weights = nmf.transform(mat[curr:(curr+x), :])
+        curr+=x
+        weightsArr[i].setWeights(weights)
+    
+    f = open('C:\\Users\\schan\\Documents\\LargeFiles\\nmf.pkl', 'wb')
+    pickle.dump(weightsArr,f,pickle.HIGHEST_PROTOCOL)
+    pickle.dump(nmf.components_, f, pickle.HIGHEST_PROTOCOL)
