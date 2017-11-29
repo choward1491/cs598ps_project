2088242365ea6cc9fb0bdf8634e170424b92dd8d
Ryley Higa <ryley.higa2@gmail.com>
2017-11-28 23:16:08 -0600

    Added time cropping transform to dataset

--- scripts/dnn_regression.py
+++ scripts/dnn_regression.py
@@ -4,6 +4,23 @@ from feature_extraction import Weights
 import numpy as np
 import pdb
 
+class TimeCrop(object):
+    '''
+    Crop file along the time dimension so that all files are the same length.
+    Time axis is assumed to be 0 (1st axis) of sample.
+    '''
+    def __init__(self, size, random = True):
+        self.random = random
+        self.size = size
+
+    def __call__(self, sample):
+        assert sample.shape[0] >= self.size
+        if self.random:
+            start = np.random.randint(0, sample.shape[0] - self.size)
+        else:
+            start = 0
+        return sample[start : start + self.size]
+
 class PatientDataset(Dataset):
     def __init__(self, file, transform=None):
         '''
@@ -27,7 +44,7 @@ class PatientDataset(Dataset):
         return {'label': label, 'features' : features}
 
 def main():
-    dataset = PatientDataset('../data/nmf.pkl')
+    dataset = PatientDataset('../data/nmf.pkl', transform = TimeCrop(100))
     print('Length of Dataset', len(dataset))
     patient_data = dataset[np.random.randint(0, len(dataset))]
     label = patient_data['label']
