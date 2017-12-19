137514ad330f4a4614d6dc3ef66be7f4e4fc60ac
Christian Howard <choward1491@gmail.com>
2017-12-19 01:31:32 -0600

    Pushing initial report write up. Note that changed LDA with GDA because realized I was never doing LDA.

--- scripts/GDA_classification.py
+++ scripts/GDA_classification.py
@@ -6,10 +6,11 @@ import myml.data_separation as mydata
 import myml.images          as myimg
 import myml.factorizations  as myfac
 import matplotlib.pyplot    as plot
+import time
 from mpl_toolkits.mplot3d import Axes3D
 
 
-def lda_model_accuracy(Xraw, L, num_features, percent_train=0.5):
+def gda_model_accuracy(Xraw, L, num_features, percent_train=0.5):
 
     # reduce dimensions of raw data
     (Q, X) = myfac.projrep(Xraw, k_or_tol=num_features)
@@ -20,8 +21,16 @@ def lda_model_accuracy(Xraw, L, num_features, percent_train=0.5):
     # break data into training and testing datasets
     (Dtr, Dtt) = mydata.separateClassData(X, L.reshape(1, nl), numdata_or_percent_for_training=percent_train)
 
+
+    # get the start time the training starts
+    start = time.time()
+
     # build the set of LDA models
     dlist = mydsc.constructDistriminantSet(X=Dtr['net'], L=Dtr['nlbl'])
+
+    # get the end time after training
+    end = time.time()
+    print('({0}) Time elapsed to train is: '.format(num_features),end-start)
 
     # test the discriminants for each label dataset in the training set
     Ltest = mydsc.evalDiscriminantSet(X=Dtt['net'], discriminant_list=dlist)
@@ -35,13 +44,13 @@ def lda_model_accuracy(Xraw, L, num_features, percent_train=0.5):
     (f0, ax0) = myimg.plotDataset(f0, C, delta=(-1, -1))
     ax0.set_xlabel('Classified Classes')
     ax0.set_ylabel('True Classes')
-    f0.savefig('{0}/confusion_mat_{1}.png'.format('../plots/LDA',num_features))
+    f0.savefig('{0}/confusion_mat_{1}.png'.format('../plots/GDA',num_features))
 
     # print output message
     print('Accuracy for {0} features is : '.format(num_features), accuracy)
 
     # return the accuracy
-    return accuracy
+    return accuracy/100.0
 
 if __name__ == '__main__':
 
@@ -63,11 +72,11 @@ if __name__ == '__main__':
     klist = np.arange(2,24,2)
     alist = np.zeros(klist.shape)
     for i in range(0,klist.shape[0]):
-        alist[i] = lda_model_accuracy(Xraw,L,klist[i])
+        alist[i] = gda_model_accuracy(Xraw,L,klist[i])
 
     # plot the results
     fig = plot.figure()
     plot.plot(klist,alist)
     plot.xlabel('Number of Features')
-    plot.ylabel('Overall Testing Classification Accuracy (%)')
-    fig.savefig('{0}/fdim_vs_accuracy.png'.format('../plots/LDA'))
+    plot.ylabel('Testing Classification Accuracy')
+    fig.savefig('{0}/fdim_vs_accuracy.png'.format('../plots/GDA'))
